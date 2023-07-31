de1e557d3f652862ca18901e9c8fcf3179dca6f5
bora <nbora617@naver.com>
2023-07-31 17:40:10 +0900

    반응형 웹 슬라이드

--- script/script.js
+++ script/script.js
@@ -32,17 +32,17 @@ close.addEventListener('click',function(){
 //const 변수명 = new Swiper('적용대상',{속성:값, 속성:값});
 const pawin_slide = new Swiper('#pawin_slide', {
     autoplay:{
-        delay:1000, //슬라이드 간격(밀리초) 자동재생 (1000=1s) (기본값:3초)
+        delay:10000, //슬라이드 간격(밀리초) 자동재생 (1000=1s) (기본값:3초)
         disableOnInteraction:false, //버튼 클릭 후 자동재생유지
     }, 
     loop:true, //마지막 슬라이드 -> 첫번째 슬라이드 자연스러운 변경 (기본값: false)
-    effect:'fade', //제자리 변경 슬라이드 효과
-    // direction:'vertical'    //슬라이드 방향 설정 = 	'horizontal' | 'vertical' (기본값:'horizontal')
+    // effect:'fade', //제자리 변경 슬라이드 효과
+    direction:'horizontal',    //슬라이드 방향 설정 = 	'horizontal' | 'vertical' (기본값:'horizontal')
     navigation: {   // 이전, 다음 네비게이션 연결
         /* next,prev 객체 연결 시 부모를 안적으면 body 안에 있는 모든 swiper-next,prev를
         인식하기 때문에 개별인식가능한 부모이름을 반드시 앞에 먼저 작성*/
     nextEl: '#pawin_slide .swiper-button-next',
-    prevEl: '#pawin_slide.swiper-button-prev',
+    prevEl: '#pawin_slide .swiper-button-prev',
     },
 });
 const pawin_slide2 = new Swiper('#pawin_slide2',{
